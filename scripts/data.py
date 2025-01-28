91db152ff6d2f367711c6cae80cf5ec1d2c37fcb
Rochakdh <rochakdh@gmail.com>
2025-01-28 01:30:35 -0500

    [Dataset] Check

--- scripts/data.py
+++ scripts/data.py
@@ -9,11 +9,15 @@ class CustomUltrasoundDataset(Dataset):
         self.image_dir = image_dir
 
     def __len__(self):
-        return len(self.hc_info)
+        return len(self.hc_df)
 
     def __getitem__(self, idx):
-        image_path = os.path.join(self.image_dir,df.iloc[idx,0])
-        segmentation_mask = os.path.join(self.image_dir,f"{self.hc_df.iloc[1,0].split(".")[0]}_Annonation.{self.hc_df.iloc[1,0].split(".")[1]}")
-        return image, segmentation_mask
+        image_path = os.path.join(self.image_dir,self.hc_df.iloc[idx,0])
+        segmentation_mask = os.path.join(self.image_dir,f"{self.hc_df.iloc[idx,0].split(".")[0]}_Annonation.{self.hc_df.iloc[idx,0].split(".")[1]}")
+        return image_path, segmentation_mask
+    
 
-
+annonation_file = "./src/training_set_pixel_size_and_HC.csv"
+img_dir = "./src/training_set"
+c = CustomUltrasoundDataset(annonation_file, img_dir)
+print(c[3])

377a7e4b0a69d29c9c1819f54e8faef5762f5bc1
Rochakdh <rochakdh@gmail.com>
2025-02-16 00:57:49 -0500

    [Added] HC18 Model

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 from scripts.data import CustomUltrasoundDataset
+from config import *
+
+class HC18US:
+    def __init__(self, dataset, batch_size=BATCH_SIZE, num_epochs=NUM_EPOCHS, lr = LEARNING_RATE, num_fold=FOLD,device=DEVICE):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.lr = lr
+        self.device = device
+        self.num_fold = num_fold
--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-DEVICE = 'cpu'
+import torch
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 SEED =  42
 BATCH_SIZE = 2
 NUM_EPOCHS = 100
 LEARNING_RATE = 0.001
+FOLD = 5
 TRAIN_DATA_PATH = "./src/train_set/"
 TEST_DATA_PATH = "./src/test_set/"
 SAVE_MODEL_PATH = './model/model.pth'
